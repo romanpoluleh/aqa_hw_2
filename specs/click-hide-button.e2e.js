1756183b2f0e08745de11f64304ba0b5d156ca25
romanpoluleh <89259195+romanpoluleh@users.noreply.github.com>
2021-11-18 18:12:57 +0300

    added test for hidden button

--- specs/click-hide-button.e2e.js
+++ specs/click-hide-button.e2e.js
@@ -1,14 +1,19 @@
-// REMOVE THE BELOW CODE BEFORE START THE EXERCISE
 describe('Check app', function () {
     it('click on hide button', async function () {
         await browser.url('https://viktor-silakov.github.io/course-sut');
         await $('#login').setValue('walker@jw.com');
         await $('#password').setValue('password');
         await $('button').click();
+        await browser.pause(5000);
+        const header = await $('.sticky-top');
+        await browser.execute(async function (a) {
+             await a.remove();
+        }, header)
         await browser.pause(3000);
-        await browser.execute('alert(arguments[0] + arguments[1])', 'Hello', ' World!');
+        await $('.btn-danger').click();
+        await browser.pause(3000);
+        await browser.acceptAlert();
         await browser.pause(3000);
     });
 });
-
 
